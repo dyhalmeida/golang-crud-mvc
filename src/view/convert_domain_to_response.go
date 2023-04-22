e10d283b1d3f71948705f01ed380cb01b19acc80
dyhalmeida <dyhalmeida@gmail.com>
2023-04-22 15:38:13 -0300

    feat: add view

---
+++ src/view/convert_domain_to_response.go
@@ -0,0 +1,15 @@
+package view
+
+import (
+	"github.com/dyhalmeida/golang-crud-mvc/src/controller/model/response"
+	"github.com/dyhalmeida/golang-crud-mvc/src/model/core/domain"
+)
+
+func ConvertDomainToResponse(userDomain domain.UserDomainInterface) response.UserResponse {
+	return response.UserResponse{
+		ID: "",
+		Email: userDomain.GetEmail(),
+		Name: userDomain.GetName(),
+		Age: userDomain.GetAge(),
+	}
+}
